1aeeb7bdc18a96477608c474d142085b8715ab29
zokirjonmurotov <zokirmurotov863@gmail.com>
2023-05-27 16:45:13 +0500

    2620 counter

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 // 2619. Array Prototype Last
-Array.prototype.last = function() {
-    return this.length === 0 ? -1 : this[this.length - 1];
+Array.prototype.last = function () {
+  return this.length === 0 ? -1 : this[this.length - 1];
 };
 
 /**
-* const arr = [1, 2, 3];
-* arr.last(); // 3
-*/
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
 
 // =======================================
+
+// 2620. Counter
+
+function createCounter(n) {
+  let count = n - 1;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+
+// ======================================
