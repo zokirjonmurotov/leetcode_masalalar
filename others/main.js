769496461a73061ecdb1f56f8b8c78b397dfcb9b
zokirjonmurotov <zokirmurotov863@gmail.com>
2023-07-22 03:28:21 +0500

    first solution

---
+++ others/main.js
@@ -0,0 +1,10 @@
+//custom map method for array
+
+Array.prototype.myMap = function (callback) {
+  let mapArray = [];
+  for (let i = 0; i < this.length; i++) {
+    let result = callback(this[i]);
+    mapArray.push(result);
+  }
+  return mapArray;
+};
