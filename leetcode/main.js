65bd96d31a7f83c51f0b2241fedcfef8eb641acd
zokirjonmurotov <zokirmurotov863@gmail.com>
2023-11-01 01:48:32 +0500

    new resolve

--- leetcode/main.js
+++ leetcode/main.js
@@ -8,31 +8,3 @@ var createHelloWorld = function () {
   };
 };
 
-
-
-//7. Reverse Integer
-
-function reverseInt(x) {
-  let newX = String(x);
-  let a = "";
-  if (x < 0) a = a.replace("-", "");
-
-  for (let i = newX.length - 1; i >= 0; i--) {
-    a += newX[i];
-  }
-  if (a[0] == "0") {
-    let j = 0;
-    let tr = true;
-    while (tr) {
-      if (a[j] != "0") {
-        a = a.replace("0", "");
-        j++;
-      } else tr = false;
-    }
-  }
-  a = x > 0 ? +a : +Number(a.replace("-", "")) * -1;
-  if (a > 2 ** 31 - 1 && a < -(2 ** 31)) return 0;
-  return a;
-}
-
-console.log(reverseInt(-605090));
---
+++ leetcode/smallestMultiple.js
@@ -0,0 +1,9 @@
+// 2413. Smallest Even Multiple
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    return n%2==0 ? n : n*2;
+};
